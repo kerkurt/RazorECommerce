fe16a45083ce864a50513852121ea858beecc918
kerkurt <106493519+kerkurt@users.noreply.github.com>
2022-06-16 23:43:41 +0300

    Add project files.

---
+++ RazorECommerce/Data/ApplicationDbContext.cs
@@ -0,0 +1,14 @@
+﻿using RazorECommerce.Model;
+using Microsoft.EntityFrameworkCore;
+
+namespace RazorECommerce.Data
+{
+    public class ApplicationDbContext : DbContext
+    {
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options) : base(options)
+        {
+
+        }
+        public DbSet<Category> Category { get; set; }
+    }
+}
