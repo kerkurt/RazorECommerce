9c71b3499f520167b505bab1aa0ed1f4514f199f
kerkurt <106493519+kerkurt@users.noreply.github.com>
2022-07-14 17:48:43 +0300

    Create category page added, worked with data annotations, validations and more.

--- RazorECommerce/Model/Category.cs
+++ RazorECommerce/Model/Category.cs
@@ -6,8 +6,9 @@ namespace RazorECommerce.Model
     {
         [Key]
         public int Id { get; set; }
-        [Required]
+        [Required, Display(Name="Category Name")]
         public string Name { get; set; }
+        [Display(Name="Display Order"), Range(1, 100, ErrorMessage = "'Display Order' must be in range of 1-100!")]
         public int DisplayOrder { get; set; }
     }
 }
---
+++ RazorECommerce/Pages/Categories/Create.cshtml.cs
@@ -0,0 +1,41 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using RazorECommerce.Data;
+using RazorECommerce.Model;
+
+namespace RazorECommerce.Pages.Categories
+{
+    //If we use like that, no need to write every single property bind attribute
+    //[BindProperties] 
+    public class CreateModel : PageModel
+    {
+        public readonly ApplicationDbContext m_Db;
+        [BindProperty]
+        public Category Category { get; set; }
+
+        public CreateModel(ApplicationDbContext db)
+        {
+            m_Db = db;
+        }
+    
+        public void OnGet()
+        {
+        }
+
+        public async Task<IActionResult> OnPost() 
+        {
+            if (Category.Name == Category.DisplayOrder.ToString())
+            {
+                ModelState.AddModelError(String.Empty, "The 'DisplayOrder' cannot exactly match the 'Name'.");
+            }
+            if (ModelState.IsValid)
+            {
+                //If there will be multiple post action methods, name can be like "OnPostCreate", "OnPostEdit" etc.
+                await m_Db.Category.AddAsync(Category);
+                await m_Db.SaveChangesAsync();
+                return RedirectToPage("Index");
+            }
+            return Page();
+        }
+    }
+}
